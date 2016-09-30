44ea4acd38589cc41cd8caddc5154c24f642fb5c
prayzzz <prayzzz@outlook.com>
2016-09-30 23:31:53 +0200

    - started tests

---
+++ test/UniversalDbUpdater.MySql.Test/Commands/BackupCommandTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using UniversalDbUpdater.Common;
+using UniversalDbUpdater.MySql.Commands;
+
+namespace UniversalDbUpdater.MySql.Test.Commands
+{
+    [TestFixture]
+    public class BackupCommandTest
+    {
+        [Test]
+        public void Test_Type()
+        {
+            Assert.AreEqual(DatabaseType.MySql, BackupCommand.Current.DatabaseType);
+        }
+
+        [Test]
+        public void Test_Backup()
+        {
+            var command = BackupCommand.Current;
+
+            command.Execute(new List<string>(), Setup.Settings);
+
+            Assert.True(Directory.Exists(Setup.Settings.BackupDirectory));
+            Assert.AreEqual(Directory.GetFiles(Setup.Settings.BackupDirectory).Length, 1);
+        }
+    }
+}
---
+++ test/UniversalDbUpdater.MySql.Test/Setup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using MySql.Data.MySqlClient;
+using NUnit.Framework;
+using UniversalDbUpdater.Common;
+
+namespace UniversalDbUpdater.MySql.Test
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        //database={settings.Database};
+        private const string ConnectionString = "server=127.0.0.1;port=3306;uid=root;pwd=admin;";
+        private const string DbName = "UniversalDbUpdaterTest";
+
+        public static readonly Settings Settings;
+
+        static Setup()
+        {
+            Settings = new Settings
+            {
+                Host = "127.0.0.1",
+                Port = 3306,
+                User = "admin",
+                Password = "root",
+                Database = DbName,
+                BackupDirectory = "./backup"
+            };
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                if (IsDatabaseAvailable(connection, DbName))
+                {
+                    using (var command = new MySqlCommand($"DROP DATABASE {DbName}", connection))
+                    {
+                        var executeScalar = command.ExecuteNonQuery();
+
+                        if (executeScalar < 1)
+                        {
+                            Console.WriteLine($"Unexpected error: {executeScalar}");
+                            return;
+                        }
+
+                        Console.WriteLine("Dropped existing Database");
+                    }
+                }
+
+                using (var command = new MySqlCommand($"CREATE DATABASE {DbName}", connection))
+                {
+                    var executeScalar = command.ExecuteNonQuery();
+
+                    if (executeScalar < 1)
+                    {
+                        Console.WriteLine($"Unexpected error: {executeScalar}");
+                        return;
+                    }
+
+                    Console.WriteLine("Created Database");
+                }
+            }
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Directory.Delete(Settings.BackupDirectory, true);
+
+            using (var connection = new MySqlConnection(ConnectionString))
+            using (var command = new MySqlCommand($"DROP DATABASE {DbName}", connection))
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private static bool IsDatabaseAvailable(MySqlConnection connection, string name)
+        {
+            using (var command = new MySqlCommand($"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{name}'", connection))
+            {
+                return command.ExecuteScalar() != null;
+            }
+        }
+    }
+}
